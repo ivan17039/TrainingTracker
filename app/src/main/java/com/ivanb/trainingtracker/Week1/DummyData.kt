193d8bb87242941973aa3b45002f3025c4c26233
ivan17039 <ivanbarada1703@gmail.com>
2026-09-10 19:13:06 +0200

    Date format changed

--- app/src/main/java/com/ivanb/trainingtracker/Week1/DummyData.kt
+++ app/src/main/java/com/ivanb/trainingtracker/Week1/DummyData.kt
@@ -12,7 +12,7 @@ object DummyData {
         Workout(
             id = 1,
             name = "Push Day",
-            dateMillis = System.currentTimeMillis(),
+            dateMillis = System.currentTimeMillis()- (1 * ONE_DAY_MS),
             exercises = listOf(
                 Exercise("Bench Press", sets = 4, reps = 8, weightKg = 60.0),
                 Exercise("Shoulder Press", sets = 3, reps = 10, weightKg = 25.0)
@@ -21,7 +21,7 @@ object DummyData {
         Workout(
             id = 2,
             name = "Pull Day",
-            dateMillis = System.currentTimeMillis() - ONE_DAY_MS,
+            dateMillis = System.currentTimeMillis() - (2 * ONE_DAY_MS),
             exercises = listOf(
                 Exercise("Deadlift", sets = 3, reps = 5, weightKg = 100.0),
                 Exercise("Pull-up", sets = 4, reps = 8, weightKg = 0.0)
@@ -30,7 +30,7 @@ object DummyData {
         Workout(
             id = 3,
             name = "Leg Day",
-            dateMillis = System.currentTimeMillis() - (2 * ONE_DAY_MS),
+            dateMillis = System.currentTimeMillis() - (3 * ONE_DAY_MS),
             exercises = listOf(
                 Exercise("Squat", sets = 4, reps = 6, weightKg = 80.0)
             )
@@ -38,7 +38,7 @@ object DummyData {
         Workout(
             id = 4,
             name = "Rest Day",
-            dateMillis = System.currentTimeMillis() - (3 * ONE_DAY_MS),
+            dateMillis = System.currentTimeMillis() - (4 * ONE_DAY_MS),
             exercises = listOf()
         )
     )
